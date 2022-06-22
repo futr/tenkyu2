1b7d9d4f70a591a0e7105999feae7f1891ba57f4
Masato Takahashi <ilce.ma@gmail.com>
2022-06-22 19:44:14 +0900

    Replace deprecated function

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QTranslator qtTranslator;
-    if ( !qtTranslator.load( QLocale(), QLatin1String( "qt" ), QLatin1String( "_" ), QLibraryInfo::location( QLibraryInfo::TranslationsPath ), QLatin1String( ".qm" ) ) ) {
+    if ( !qtTranslator.load( QLocale(), QLatin1String( "qt" ), QLatin1String( "_" ), QLibraryInfo::path( QLibraryInfo::TranslationsPath ), QLatin1String( ".qm" ) ) ) {
         qtTranslator.load( QLocale(), QLatin1String( "qt" ), QLatin1String( "_" ), QLatin1String( "./translations" ), QLatin1String( ".qm" ) );
     }
     a.installTranslator( &qtTranslator );
 
     QTranslator myappTranslator;
-    if ( !myappTranslator.load( QLocale(), QLatin1String( "tenkyu" ), QLatin1String( "_" ), QLibraryInfo::location( QLibraryInfo::TranslationsPath ), QLatin1String( ".qm" ) ) ) {
+    if ( !myappTranslator.load( QLocale(), QLatin1String( "tenkyu" ), QLatin1String( "_" ), QLibraryInfo::path( QLibraryInfo::TranslationsPath ), QLatin1String( ".qm" ) ) ) {
         if ( !myappTranslator.load( QLocale(), QLatin1String( "tenkyu" ), QLatin1String( "_" ), QLatin1String( "./translations" ), QLatin1String( ".qm" ) ) ) {
             myappTranslator.load( QLocale(), QLatin1String( "tenkyu" ), QLatin1String( "_" ), QLatin1String( ":/translations" ), QLatin1String( ".qm" ) );
         }
